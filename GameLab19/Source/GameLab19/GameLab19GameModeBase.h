4ac96d262301bd157fe5bc067b05a6bed343da13
Sam D <sdifeo1@gmail.com>
2019-01-30 11:20:49 -0500

    Initial Unreal setup

---
+++ GameLab19/Source/GameLab19/GameLab19GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameLab19GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMELAB19_API AGameLab19GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
