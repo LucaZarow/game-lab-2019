8aa2506d6c437f8753f554e775957f4f65e5f94d
PierrC <PierrC@users.noreply.github.com>
2019-02-19 14:53:55 -0500

    Almost done with BaseActorBlueprint

---
+++ GameLab19/Source/GameLab19/Public/Damagable.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Damagable.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE(MinimalAPI)
+class UDamagable : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * 
+ */
+class GAMELAB19_API IDamagable
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	/**
+		Adds input to health
+	*/
+	UFUNCTION(BlueprintImplementableEvent, Category = "Damagable")
+	void CalculateHealth(int addition);
+
+};
